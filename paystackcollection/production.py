9cc29136754f4d2223b62f7de0de920bc11bbbb3
medayola <medayola@yahoo.com>
2021-08-20 09:15:04 -0400

    quick update

--- paystackcollection/production.py
+++ paystackcollection/production.py
@@ -207,6 +207,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost",
+    "http://localhost:3002"
     "https://localhost",
     "http://159.65.37.127",
     "https://159.65.37.127",
