1844562c5fcc1546994a958b2ae2ddd3066e12c9
medayola <medayola@yahoo.com>
2021-08-25 07:06:09 -0400

    new update

--- paystackcollection/staging.py
+++ paystackcollection/staging.py
@@ -216,6 +216,7 @@ PAYSTACK_INITIAL_FEE = 5000  # 50 NGN
 
 RENTAL_WIDTH_IN_DAYS = 60
 FRONT_END = 'http://157.245.8.141/collections'
+#FRONT_END = 'https://treasury-dev.concept-nova.com/collections'
 
 STATIC_URL = '/collections.static/'
 MEDIA_URL = '/collections.media/'
